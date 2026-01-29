b8b94aeba362b907deeb95a2acab0781813bbe62
Ashmit <ashmitpandey04@gmail.com>
2026-01-29 15:09:33 +0530

    added mobile navigation

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AskAQuestion = () => {
+    return (
+        <div>
+            <h1>Ask A Question</h1>
+        </div>
+    );
+};
+
+export default AskAQuestion;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,15 @@ const HomeLayout = ({children}:{children:ReactNode}) => {
   return (
     <main>
       <Navbar/>
-      {children}
+      <div className='flex'>
+
+            <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
+                <div className='mx-auto w-full max-w-5xl'>
+                    {children} 
+                 </div>
+
+            </section> 
+      </div>
       
     </main>
   )
