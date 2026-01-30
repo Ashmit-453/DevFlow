6dbfad23f8a1695d6a74c6bd0858d4998eb9234d
Ashmit <ashmitpandey04@gmail.com>
2026-01-30 17:37:24 +0530

    question card setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import ROUTES from '@/constants/routes';
 import Link from 'next/link';
 import LocalSearch from '@/components/search/LocalSearch';
 import HomeFilter from '@/components/filters/HomeFilter'; 
-
+import QuestionCard from '@/components/cards/QuestionCard';
 const questions = [
   {
     _id: "1",
@@ -14,7 +14,10 @@ const questions = [
       { _id: "1", name: "React" },
       { _id: "2", name: "JavaScript" },
     ],
-    author: { _id: "1", name: "John Doe" },
+    author: { _id: "1", name: "John Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+     },
     upvotes: 10,
     answers: 5,
     views: 100,
@@ -28,8 +31,11 @@ const questions = [
       { _id: "1", name: "JavaScript" },
       { _id: "2", name: "JavaScript" },
     ],
-    author: { _id: "1", name: "John Doe" },
-    upvotes: 10,
+    author: { _id: "1", name: "John Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+    upvotes: 10, },
+    
     answers: 5,
     views: 100,
     createdAt: new Date(),
@@ -68,7 +74,7 @@ const Home =  async({searchParams}:SearchParams) => {
     <HomeFilter/>
     <div className='mt-10 flex w-full flex-col gap-6'>
       { filteredQuestions.map((question) =>(
-        <h1 key ={question._id}>{question.title}</h1>
+        <QuestionCard key ={question._id} question={question} />
       )
       )}
     </div>
