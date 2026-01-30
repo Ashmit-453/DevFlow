976cb905ea2f1b032922796a73d4707fea33e52b
Ashmit <ashmitpandey04@gmail.com>
2026-01-30 14:13:56 +0530

    implemented filtering of questions on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,70 @@
 import { auth, signOut } from '@/auth';
 import { Button } from '@/components/ui/button';
 import ROUTES from '@/constants/routes';
-const Home = async () => {
-  const session = await auth();
-  const buttonClass = "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5 cursor-pointer"
-  console.log("Session:", session);
-  return (
-    <form className='px-10 pt-[100px]' action={async () => {
-      "use server";
-      await signOut({ redirectTo: ROUTES.SIGN_IN});
-    }}>
-      <Button className={buttonClass} type='submit'>Log Out </Button>
+import Link from 'next/link';
+import LocalSearch from '@/components/search/LocalSearch';
 
-    </form>
 
-  );
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string}>
 }
+const Home =  async({searchParams}:SearchParams) => {
+  const { query = "" } = await searchParams;
+  const filteredQuestions = questions.filter(( question ) => 
+  question.title.toLowerCase().includes(query?.toLowerCase()) );
+  return (
+    <>
+    <section className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+      <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
+      <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900' asChild>
+        <Link href={ROUTES.ASK_QUESTION}> Ask a Question </Link> 
+      </Button> 
+    </section>
+    <section className='mt-11'>
+      <LocalSearch
+        route="/"
+        imgSrc="/icons/search.svg"
+        placeholder="Search questions..." 
+        otherClasses="flex-1"      
+      />
+    </section>
+    <div className='mt-10 flex w-full flex-col gap-6'>
+      { filteredQuestions.map((question) =>(
+        <h1 key ={question._id}>{question.title}</h1>
+      )
+      )}
+    </div>
+  </>
+);
+};
 export default Home;
