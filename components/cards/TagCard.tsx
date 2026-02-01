68d038ebb6af638dca25c26b6498425d33f20670
Ashmit <ashmitpandey04@gmail.com>
2026-02-01 17:41:32 +0530

    implemented adding multiple tags

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import {Badge} from '../ui/badge';
 import ROUTES from '@/constants/routes'; 
 import { getDeviconClassName } from '@/lib/utils';
+import Image from 'next/image'
+import { X } from 'lucide-react';
 
 interface Props {
     _id: string;
@@ -10,27 +12,54 @@ interface Props {
     questions?: number;
     showCount?: boolean;
     compack?: boolean;
+    remove?: boolean;
+    isButton?:boolean;
+    handleRemove?: () => void;
 }
 
-const TagCard = ({ _id,name,questions,showCount,compack }: Props) => {
+const TagCard = ({ _id,name,questions,showCount,compack,remove,isButton,handleRemove }: Props) => {
     const iconClass = getDeviconClassName(name);
-    return (
-         <Link href={ROUTES.TAGS(_id)} className='flex justify-between gap-2'>
-            <Badge className='subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase'>
+
+    const handleClick = (e: React.MouseEvent) => {
+        e.preventDefault();
+    }
+
+    const Content = (
+        <>
+         <Badge className='subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase'>
                 <div className='flex-center space-x-2'>
                     <i className={`${iconClass} text-sm`}></i>
                     <span>{name}</span> 
                 </div>  
-            </Badge>
-        
-        {
-          showCount && (
-            <p className='small-medium text-dark500_light700'>{questions}</p>
-          )  
-        }
-         
-         </Link>
+
+    {remove && (
+        <X
+        size={12}
+        className="cursor-pointer text-dark-400 dark:text-light-700"
+        onClick={handleRemove}
+        />
+    )} 
+    </Badge>
+
+    { showCount && (
+      <p className='small-medium text-dark500_light700'>{questions}</p>
+          )}       
+        </>
     );
+
+
+    if (compack) {
+        return isButton ? (
+            <button onClick={handleClick} className='flex justify-between gap-2'>
+                    {Content}
+            </button>
+        ) : (
+        <Link href={ROUTES.TAGS(_id)} className='flex justify-between gap-2'> 
+        {Content}
+        </Link>
+        )
+    }
+ 
 };
 
 export default TagCard;
--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -19,13 +19,14 @@ import { AskQuestionSchema } from "@/lib/validations";
 import { title } from "process";
 import dynamic from "next/dynamic";
 import { MDXEditorMethods } from "@mdxeditor/editor";
- 
+import z from "zod"; 
+import TagCard from "../cards/TagCard";
 const Editor = dynamic(() => import("@/components/editor"), {
   ssr: false,
 });
 const QuestionForm = () => {
   const editorRef = useRef<MDXEditorMethods>(null); 
-  const form = useForm({
+  const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
       title: "",
@@ -33,8 +34,45 @@ const QuestionForm = () => {
       tags: [],
     },
   });
+ const handleInputKeyDown = ( e: React.KeyboardEvent<HTMLInputElement>,
+    field: { value: string[]}
+  ) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        const tagInput = e.currentTarget.value.trim();
 
-  const handleCreateQuestion = () => {};
+        if (tagInput && tagInput.length < 15 && !field.value.includes(tagInput)) {
+          form.setValue("tags",[...field.value, tagInput]);
+          e.currentTarget.value = "";
+          form.clearErrors("tags");
+        } else if(tagInput.length > 15) {
+          form.setError("tags", {
+            type: "manual",
+            message: "Tag should be less then 15 characters",
+          });
+        } else if (field.value.includes(tagInput)) {
+          form.setError("tags", {
+            type: "manual",
+            message: "Tag Already exists"
+          });
+        }
+      }
+  };
+  const handleTagRemove = (tag: string, field: { value: string[] }) => {
+    const newTags = field.value.filter((t) => t !== tag);
+
+    form.setValue("tags", newTags);
+    if (newTags.length === 0) {
+      form.setError("tags",{
+        type: "manual",
+        message: "Tags are required"
+      }); 
+    }
+  }
+
+  const handleCreateQuestion = (data: z.infer<typeof AskQuestionSchema>) => {
+    console.log(data);
+  };
   
 
   return (
@@ -102,10 +140,25 @@ const QuestionForm = () => {
                   <Input
                     className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
                     placeholder="Add tags..."
-                    {...field}
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
                   />
                   <br/>
-                  Tags
+                  {field.value.length > 0 && (
+            <div className="flex-start mt-2.5 flex-wrap gap-3">
+              {field?.value?.map((tag:string) => (
+                <TagCard
+                  key={tag}
+                  _id={tag}
+                  name={tag}
+                  compack
+                  remove
+                  isButton
+                  handleRemove = {() => handleTagRemove(tag,field)} 
+                  />
+              ))}
+
+            </div>
+          )}
                 </div>
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
